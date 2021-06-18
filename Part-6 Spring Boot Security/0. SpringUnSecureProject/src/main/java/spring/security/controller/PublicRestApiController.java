36367c10a488bd584d1100a7a480a517e8a22f48
superman19993 <73101805+superman19993@users.noreply.github.com>
2021-06-18 10:42:18 +0700

    spring security dependcies and configuration file added

--- Part-6 Spring Boot Security/0. SpringUnSecureProject/src/main/java/spring/security/controller/PublicRestApiController.java
+++ Part-6 Spring Boot Security/0. SpringUnSecureProject/src/main/java/spring/security/controller/PublicRestApiController.java
@@ -3,6 +3,7 @@ package spring.security.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @Created 14 / 04 / 2020 - 3:27 PM
@@ -15,11 +16,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PublicRestApiController {
 
     @GetMapping("test1")
+    @ResponseBody
     public String index(){
         return "testing 1";
     }
 
     @GetMapping("test2")
+    @ResponseBody
     public String index2(){
         return "API Test 2";
     }
